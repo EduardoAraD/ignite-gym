b7dd7df7bdae05b9b3abed800e9323cedf983684
eduardoarad <araujocarlos893@gmail.com>
2023-07-17 14:42:17 -0300

    Autenticacao JWT

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -5,6 +5,7 @@ import { api } from "@services/api";
 import { UserDTO } from "@dtos/UserDTO";
 
 import { storageUserSave, storageUserGet, storageUserRemove } from "@storage/storageUser";
+import { storageAuthTokenSave, storageAuthTokenGet, storageAuthTokenRemove } from '@storage/storageAuthToken';
 
 export type AuthContextDataProps = {
   user: UserDTO;
@@ -24,13 +25,33 @@ export function AuthContextProvider({ children }: AuthContextProviderProps) {
   const [user, setUser] = useState<UserDTO>({} as UserDTO);
   const [isLoadingUserStorageData, setIsLoadingUserStorageData] = useState(true);
 
+  function userAndTokenUpdate(userData: UserDTO, token: string) {
+    api.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+
+    setUser(userData);
+  }
+
+  async function storageUserAndTokenSave (userData: UserDTO, token: string) {
+    try {
+      setIsLoadingUserStorageData(true);
+
+      await storageUserSave(userData);
+      await storageAuthTokenSave(token);
+    } catch (error) {
+      throw error;
+    } finally {
+      setIsLoadingUserStorageData(false);
+    }
+  }
+
   async function signIn(email: string, password: string) {
     try {
       const { data } = await api.post('/sessions', { email, password });
 
-      if(data.user) {
-        setUser(data.user);
-        storageUserSave(data.user);
+      if(data.user && data.token) {
+        await storageUserAndTokenSave(data.user, data.token);
+        
+        userAndTokenUpdate(data.user, data.token);
       }
     } catch (error) {
       throw error;
@@ -43,6 +64,7 @@ export function AuthContextProvider({ children }: AuthContextProviderProps) {
 
       setUser({} as UserDTO);
       await storageUserRemove()
+      await storageAuthTokenRemove();
 
     } catch (error) {
       throw error;
@@ -54,12 +76,14 @@ export function AuthContextProvider({ children }: AuthContextProviderProps) {
   async function loadUserData() {
     try {
       const userLogged = await storageUserGet();
+      const token = await storageAuthTokenGet();
 
-      if(userLogged) {
-        setUser(userLogged);
+      if(userLogged && token) {
+        userAndTokenUpdate(userLogged, token);
       }
     } catch (error) {
-      throw (error);
+      throw error;
+
     } finally{
       setIsLoadingUserStorageData(false);
     }
--- src/storage/storageConfig.ts
+++ src/storage/storageConfig.ts
@@ -1,5 +1,6 @@
-const appName = '@gymignite'
+const appName = '@ignitegym';
 
+const AUTH_TOKEN_STORAGE = `${appName}:token`;
 const USER_STORAGE = `${appName}:user`;
 
-export { USER_STORAGE };
+export { USER_STORAGE, AUTH_TOKEN_STORAGE };
